02a0f2938cb5e40fadeacbba6bda9cfcee1bbccf
timmyluong17 <timmy.luong@sjsu.edu>
2026-02-27 20:34:10 -0800

    Tut 9

---
+++ tutorials/2-css/06-dom/js/main.js
@@ -0,0 +1,49 @@
+// console.log("javascript!")
+
+
+// select html elements
+const header = document.querySelector("#header");
+const changHeaderButton = document.querySelector("#change-header-button");
+const changeThemeButton = document.querySelector("#change-theme-button");
+const img1 = document.querySelector("#img1")
+const img2 = document.querySelector("#img2")
+const img3 = document.querySelector("#img3")
+
+
+
+// change header with button click
+changHeaderButton.addEventListener("click", () => {
+    header.innerHTML = 'POW!!!!'
+})
+
+
+// toggle color theme
+
+// create function for changing button text
+function changeButtonText(){
+    if (document.body.classList.contains("dark")) {
+        changeThemeButton.textContent = "Switch to Light Theme"
+    } else {
+        changeThemeButton.textContent = "Switch to Dark Theme"
+    }
+}
+
+
+// click event on button
+changeThemeButton.addEventListener("click",() => {
+    //add/remove dark class to body
+    document.body.classList.toggle("dark");
+    changeButtonText();
+})
+
+
+
+// toggle image visibility
+
+img1.addEventListener("click", () => {
+    img2.classList.remove("hidden");
+})
+
+img2.addEventListener("click", () => {
+    img3.classList.remove("hidden");
+})
