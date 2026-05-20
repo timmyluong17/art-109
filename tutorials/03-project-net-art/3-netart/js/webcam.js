091891e508424999f8a177aaff2c0c8b86d995c3
timmyluong17 <timmy.luong@sjsu.edu>
2026-05-20 12:05:14 -0700

    Gif Tutorial video and webcam

---
+++ tutorials/03-project-net-art/3-netart/js/webcam.js
@@ -0,0 +1,18 @@
+const webcamVideo = document.querySelector("#webcam");
+
+async function startWebcam() {
+try {
+    
+const stream = await navigator.mediaDevices.getUserMedia({
+video: true,
+audio: false
+});
+
+webcamVideo.srcObject = stream;
+
+} catch (error) {
+console.error("Error accessing webcam:", error);
+}
+}
+
+startWebcam();
